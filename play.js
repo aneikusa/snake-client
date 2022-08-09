cb6cdeef41c75e26e8683a717d45fe85a8a87817
Suki Abdulla <sukiena.abdulla@gmail.com>
2022-08-09 19:04:46 -0400

    Export connect function using shorthand syntax

--- play.js
+++ play.js
@@ -1,25 +1,3 @@
-const net = require("net");
+const {connect} = require("./client");
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost",
-    port:  50541
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on("data", (data) => {
-    // code that does something when the connection is first established
-    console.log(data);
-  })
-
-  return conn;
-};
-
-
-console.log("Connecting ...");
-connect();
-
-
+connect;
