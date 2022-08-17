c9b3b399176b9c34f340adbcd400d0de4f374b29
Suki Abdulla <sukiena.abdulla@gmail.com>
2022-08-17 16:46:46 -0400

    Create a function called handleUserInput and register it as the data callback handler for stdin.

--- client.js
+++ client.js
@@ -20,10 +20,23 @@ const connect = function () {
     conn.write(`Name: S.A`);
   })
 
-  // conn.on("Move: up", () => {
-  //   console.log("Move: up");
-  //   conn.write(`Move: up`);
-  // })
+  const setupInput = function () {
+    const stdin = process.stdin;
+    stdin.setRawMode(true);
+    stdin.setEncoding("utf8");
+    stdin.resume();
+    
+    stdin.on("data", handleUserInput);
+
+    return stdin;
+  };
+
+  const handleUserInput = (data) => {
+    connection.setEncoding("utf8");
+      if (data === "\u0003") {
+        process.exit();
+    }
+  };
 
   return conn;
 };
